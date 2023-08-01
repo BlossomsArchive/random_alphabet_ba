48ae7ed8221d8f1934c92c2fe3b1e361ed1fb9a8
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-08-02 00:10:42 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+from misskey import Misskey
+import os
+
+# Misskey
+misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+misskey_token = os.environ.get("MISSKEY_TOKEN")
+api = Misskey(misskey_address)
+api.token = misskey_token
+
+alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+max = random.randint(2, 5)
+post_text = ""
+for i in range (max):
+    post_text = post_text + random.choice(alphabet_list)
+
+print(post_text)
+api.notes_create(text=post_text)
