ccdb3b7b052b930c6dd60f09ba9467d421546bba
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-09-26 20:09:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from misskey import Misskey
 import os
 import time
 
-# Misskey
-misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
-misskey_token = os.environ.get("MISSKEY_TOKEN")
-api = Misskey(misskey_address)
-api.token = misskey_token
-
 alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 max = random.randint(2, 5)
@@ -19,6 +13,11 @@ for i in range (max):
 print(post_text)
 while True:
     try:
+        # Misskey
+        misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+        misskey_token = os.environ.get("MISSKEY_TOKEN")
+        api = Misskey(misskey_address)
+        api.token = misskey_token
         api.notes_create(text=post_text)
     except:
         time.sleep(300)
